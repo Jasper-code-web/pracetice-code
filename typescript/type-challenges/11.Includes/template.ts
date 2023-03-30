871c4ab275810b877601fc66bfb4bb072e7115e7
1156657702@qq.com <1156657702@qq.com>
2023-03-30 22:01:07 +0800

    includes

--- typescript/type-challenges/11.Includes/template.ts
+++ typescript/type-challenges/11.Includes/template.ts
@@ -1,7 +1,12 @@
-// 你的答案
-type MyEqual<X, Y> = (<T>() => T extends X ? 1 : 2) extends (<T>() => T extends Y ? 1 : 2) ? true : false
+import type { Equal, Expect } from '@type-challenges/utils'
 
-// 如果是两个条件类型 T1 extends U1 ? X1 : Y1 和 T2 extends U2 ? X2 : Y2 相关的话，那 T1 和 T2 相关、X1 和 X2 相关、Y1 和 Y2 相关，而 U1 和 U2 相等
+// type MyEqual<X, Y> = (<T>() => T extends X ? 1 : 2) extends (<T>() => T extends Y ? 1 : 2) ? true : false
 
+export type Includes<T extends readonly any[], U> = T extends [infer First, ...infer Rest] ? 
+Equal<First, U> extends true ? true : Includes<Rest, U> : false;
 
-type Includes<T extends readonly any[], U> = T extends [infer First, ...infer Rest] ? MyEqual<U, First> extends true ? true : Includes<Rest, U> : false
+//知识点
+//1.用递归实现遍历数组
+//2.ts的模块规范
+    //如果有export/import的话,那么就是模块
+    //如果没有那就是全局，可以直接在别的模块引用
