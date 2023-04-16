a915b6358a5555f308c781d4eaebdf80e6b75064
1156657702@qq.com <1156657702@qq.com>
2023-04-16 15:24:17 +0800

    vue3 dynamic component

--- Vue3/course/Vue3-course/src/App.vue
+++ Vue3/course/Vue3-course/src/App.vue
@@ -1,18 +1,23 @@
 <template>
   <div>
-
+    {{ re }}
+    <button @click="add">添加</button>
   </div>
 </template>
 
 <script setup lang="ts">
-import { track } from '@vue/reactivity';
-import { trigger } from '@vue/reactivity';
-import { ref, isRef, shallowRef, customRef } from 'vue';
-import type { Ref } from 'vue';
+import { ref, reactive } from 'vue';
 
+let re = reactive([1, 2])
 
-
+// setTimeout(() => {
+//   re = [1]
+//   console.log('re', re)
+// }, 2000)
+const add = () => {
+  re = [1]
+  console.log('re', re)
+}
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
