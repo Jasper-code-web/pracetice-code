1e50a8dec8c62974402e4f8a1c0269d72acd61cb
1156657702@qq.com <1156657702@qq.com>
2023-04-16 22:22:11 +0800

    algorithm Array 704

---
+++ algorithm/Array/1.702二分查找.js
@@ -0,0 +1,14 @@
+var search = function (nums, target) {
+    let left = 0, mid = 0, right = nums.length - 1
+    while(left <= right) {
+        mid = Math.floor((right + left) / 2)
+        if(nums[mid] < target) {
+            left = mid + 1 
+        } else if(nums[mid] > target) {
+            right = mid - 1
+        } else {
+            return mid
+        }
+    }
+    return -1
+}
